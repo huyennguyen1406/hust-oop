00f29a384ecddde2d5298fde36f4f66a507f62f2
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2024-10-02 20:38:31 +0700

    spinral array

---
+++ oop/src/Arrays/spiralArray.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+public class spiralArray {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] matrix = new int[n][n];
+
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int value = 1;
+
+        while (value <= n * n) {
+
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = value++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = value++;
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = value++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = value++;
+                }
+                left++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
